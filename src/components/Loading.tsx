be84ab5d0cbccfc4171c26e093c169e2fd26c9bd
1371101711970002 <farhanrafid97@gmail.com>
2022-12-27 14:17:41 +0700

    change styling with tailwind

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,11 @@
+import { ImSpinner10 } from 'react-icons/im';
+
 const Loading: React.FC = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="text-white">
+      <ImSpinner10 className="animate-spin text-[60px]" />
+    </div>
+  );
 };
 
 export default Loading;
