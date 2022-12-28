30f62495513e9a2be70ade5d203843eb4a482337
1371101711970002 <farhanrafid97@gmail.com>
2022-12-28 15:42:27 +0700

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
+  },
+  build: {
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    polyfillModulePreload: true,
+    reportCompressedSize: true,
   },
 });
